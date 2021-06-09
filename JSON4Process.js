1a9153376ac0ec12713f57b881d290bc1f5ccfdd
fuscoantonio <47628423+fuscoantonio@users.noreply.github.com>
2021-06-09 21:43:59 +0200

    Add files via upload

---
+++ JSON4Process.js
@@ -0,0 +1,97 @@
+const _ = require('lodash');
+
+const objectify = (obj, start = true) => {
+    let newObj = obj;
+    if (start)
+        newObj = _.cloneDeep(obj);
+
+    for (let prop in newObj) {
+        if (typeof newObj[prop] === 'string' && newObj[prop].match(/-\d\dT\d\d:\d\d:/)) {
+            newObj[prop] = new Date(newObj[prop]);
+        } else if (typeof newObj[prop] === 'string' && newObj[prop].match(/^\/.+\//)) {
+            let [regex, flags] = newObj[prop].split('/').slice(1);
+            newObj[prop] = new RegExp(regex, flags);
+        } else if (typeof newObj[prop] === 'string' && newObj[prop].startsWith('-this_is_a_function-')) {
+            let func = newObj[prop].slice(20);
+            let params = func.split(' ')[0].split(',');
+            let funcBody = func.split(' ').slice(1).join(' ');
+            newObj[prop] = new Function(...params, funcBody);
+        } else if (typeof newObj[prop] === 'object') {
+            objectify(newObj[prop], false);
+        }
+    }
+
+    return newObj;
+}
+
+/**
+ * Returns an object with all properties previously stringified changed back to their original data type (Function, Date, RegExp).
+ * @param {{}} obj A JavaScript object to be converted.
+ */
+const objectifyProps = (obj) => {
+    return objectify(obj);
+}
+
+
+const stringify = (obj, start = true) => {
+    let newObj = obj;
+    if (start)
+        newObj = _.cloneDeep(obj);
+
+    const getArrowFuncParams = (func) => {
+        let params;
+        if (func.indexOf('(') < func.indexOf('=>')) { //arrow function with parenthesis for params
+            params = func.slice(func.indexOf('(') + 1, func.indexOf(')')).replace(/ /g, '');
+        } else { //arrow function without parenthesis for single param
+            params = func.slice(0, func.indexOf('=>')).replace(/ /g, '');
+        }
+        return params;
+    }
+
+    const getArrowFuncBody = (func) => {
+        let funcBody;
+        if (func.indexOf('{') === -1 || func.indexOf('{') > func.slice(func.indexOf('=>') + 2).indexOf('(') && func.slice(func.indexOf('=>' + 2)).indexOf('(') !== -1) { //arrow function with one statement
+            funcBody = 'return ' + func.slice(func.indexOf('=>') + 2);
+        } else { //arrow function with multiple statements
+            funcBody = func.slice(func.indexOf('{') + 1, func.lastIndexOf('}'));
+        }
+        return funcBody;
+    }
+
+    for (let prop in newObj) {
+        if (newObj[prop] instanceof Date)
+            newObj[prop] = newObj[prop].toISOString();
+        else if (newObj[prop] instanceof RegExp)
+            newObj[prop] = newObj[prop].toString();
+        else if (newObj[prop] instanceof Function) {
+            let func = newObj[prop].toString();
+            let params, funcBody;
+            if (func.indexOf('=>') !== -1) { //arrow function
+                params = getArrowFuncParams(func);
+                funcBody = getArrowFuncBody(func);
+            } else {
+                params = func.slice(func.indexOf('(') + 1, func.indexOf(')')).replace(/ /g, '');
+                funcBody = func.slice(func.indexOf('{') + 1, func.lastIndexOf('}'));
+            }
+            newObj[prop] = '-this_is_a_function-' + params + ' ' + funcBody;
+        }
+        else if (typeof newObj[prop] === 'object')
+            stringify(newObj[prop], false);
+    }
+
+    return newObj;
+}
+
+/**
+ * Returns an object with all properties converted to strings and ready to be sent to a child process.
+ * @param {{}} obj A JavaScript object to be converted.
+ */
+const stringifyProps = (obj) => {
+    return stringify(obj);
+}
+
+
+module.exports = {
+    stringifyProps,
+    objectifyProps
+}
---
+++ README.md
@@ -0,0 +1,92 @@
+# JSON4Process
+
+[![GitHub](https://img.shields.io/badge/GitHub-v.0.1.5-red.svg)](https://github.com/fuscoantonio/JSON4Process)  
+A simple module to modify an object's properties of types such as Function, Date or RegExp to strings and back to their original data type while maintaining the object's structure. Useful when sending objects as data to forked and spawned child processes.   
+
+This module stringifies properties of these data types:
+  - **Function**
+  - **RegExp**
+  - **Date**
+
+**Import:**
+```javascript
+    const JSON4Process = require('json4process');
+```   
+**Basic usage:**
+```javascript
+    let obj = {
+    name: 'Jack',
+    dateOfBirth: new Date(),
+    regex: new RegExp(/something/g),
+    func: () => { console.log('something') },
+    parents: [
+            {
+                name: 'John',
+                dateOfBirth: new Date(),
+                regex: new RegExp(/nothing/),
+                func: function (param) {
+                    return param + 2;
+                }
+            },
+            {
+                name: 'Ashley',
+                dateOfBirth: new Date(),
+                regex: new RegExp(/anything/i),
+                func: (param, param2) => param + param2
+            }
+        ]
+    }
+
+    let convertedObj = JSON4Process.stringifyProps(obj);
+
+    //result
+    {
+        name: 'Jack',
+        dateOfBirth: '2021-06-09T18:51:12.814Z',
+        regex: '/something/g',
+        func: "-this_is_a_function-  console.log('something') ",
+        parents: [
+            {
+                name: 'John',
+                dateOfBirth: '2021-06-09T18:51:12.814Z',
+                regex: '/nothing/',
+                func: '-this_is_a_function-param \r\n' +
+                    '                return param + 2;\r\n' +
+                    '            '
+            },
+            {
+                name: 'Ashley',
+                dateOfBirth: '2021-06-09T18:51:12.814Z',
+                regex: '/anything/i',
+                func: '-this_is_a_function-param,param2 return  param + param2'
+            }
+        ]
+    }
+```
+**Usage with child_process fork** 
+After having stringified the object's properties it's ready to be sent as data to a child process.
+```javascript
+    const child = fork('filename.js');
+    child.send(convertedObj); //convertedObj is the stringified object in the example above
+```
+And so we can "objectify" back all object properties in the newly spawned process, like so
+```javascript
+    process.on('message', (data) => {
+        let newData = JSON4Process.objectifyProps(data);
+        //Function, Date and RegExp properties are back to their data type and can now be used as such
+    });
+```
+
+**Usage with spawn or exec**
+Since we need to stringify the whole object, we first stringify each property with JSON4Process and then we stringify the object itself.
+```javascript
+    //obj is the object in the first example in "Basic usage" at the top of this document
+    let convertedObj = JSON.stringify(JSON4Process.stringifyProps(obj));
+    const child = spawn('node', ['filename.js', convertedObj]);
+```
+Now we can "objectify" object's properties in the spawned process
+```javascript
+    let newData = JSON4Process.objectifyProps(JSON.parse(process.argv[2]));
+```
+
+**author:** [@fuscoantonio](https://github.com/fuscoantonio), antonio.fusco1992@gmail.com
